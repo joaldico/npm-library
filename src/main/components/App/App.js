514b4b7b956340ac510a2235ddc82f6a24a95a9f
Josue <j.diaz@bucares.com>
2020-06-16 14:53:49 -0400

    React Project

--- src/main/components/App/App.js
+++ src/main/components/App/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { ComponentTest } from 'plugbox-ui';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          <ComponentTest/>
+        <span
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          BUCARES TEAM
+        </span>
       </header>
     </div>
   );
