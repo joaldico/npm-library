1306a401a1788e2717bcb39e1eb152ab8bdd082e
Josue <j.diaz@bucares.com>
2020-06-19 08:50:58 -0400

    Modified component

--- src/main/components/App/App.js
+++ src/main/components/App/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import { ComponentTest } from 'plugbox-ui';
+import { Button } from 'plugbox-ui';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <ComponentTest 
-            name={"plugbox-ui"}
-        />
+        <h1>
+            Este es mi componente de test: {"Button"}
+          </h1>
         <span
           className="App-link"
         >
           BUCARES TEAM
         </span>
+        <div className="Container-buttons">
+          <div className="Container-buttons">
+            <Button 
+                text={"Click here to go to: plugbox-ui LIBRARY"}
+                size={"small"}
+                disabled={false}
+                onClick={() => {console.log("go to: plugbox-ui LIBRARY")}}
+            />
+          </div>
+          <div className="Container-buttons">
+            <Button 
+                text={"Click here to go to: plugbox-ui LIBRARY"}
+                size={"large"}
+                disabled={true}
+                onClick={() => {console.log("go to: plugbox-ui LIBRARY")}}
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
