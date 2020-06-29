bc378e3a5a7b5b6f9a294cae030b5501240ac2ad
Josue <j.diaz@bucares.com>
2020-06-28 22:32:42 -0400

    Removed file .npmrc

--- src/main/components/App/App.js
+++ src/main/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
-import { Button, Notification, Paragraph, Link, Checkbox, Input, Select } from 'plugbox-ui';
+import { Button, Notification, Paragraph, Link, Checkbox, Input, Select } from '@plugbox/plugbox-ui';
 
 class App extends React.Component {
 	state = {
